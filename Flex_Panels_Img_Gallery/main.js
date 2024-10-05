62c9ca20274c1e4f1a5b2ec41827b47b04d38635
theLazyBoy_mmTu <maiminhtu130803@gmail.com>
2024-10-05 22:03:43 +0700

    fix lỗi user click trùng trong project Flex Panels Img Gallery

--- Flex_Panels_Img_Gallery/main.js
+++ Flex_Panels_Img_Gallery/main.js
@@ -1,6 +1,11 @@
 const panels = document.querySelectorAll(".panel");
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
+    // Kiểm tra nếu panel hiện tại đã có lớp 'open' thì giữ nguyên, không làm gì cả
+    if (panel.classList.contains("open")) {
+      return;
+    }
+    // Xóa lớp 'open' và 'open-active' khỏi tất cả các panel khác
     panels.forEach((p) => {
       p.classList.remove("open");
       p.classList.remove("open-active");
